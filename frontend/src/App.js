00538eaa55be56ca77cf31f07c9b451e7a3a4923
rommel pabustan <Godservant14@gmail.com>
2022-03-27 22:26:25 +0800

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Header from './components/Header';
-//import Search from './components/Search';
-import Title from './Title.js';
+import Header from './components/Header';
+import Search from './components/Search';
 
 
-//const App = () => {
-  //const [word, setWord] = useState('');
+const App = () => {
+const [word, setWord] = useState('');
 
- //const handleSearchSubmit = (e) => {
-  // e.preventDefault();
- // console.log(word);
-//}
+ const handleSearchSubmit = (e) => {
+  e.preventDefault();
+ console.log(word);
+}
 
- //console.log(word);
+ console.log(word);
 
-//return (
-   //<div>
-  //  <Header title="Bulacan State University"/> 
+return (
+  <div>
+ <Header title="Bulacan State University"/> 
      
-  //  <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
-  //  </div>
+   <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+   </div>
 
     
-// );
-//}
+);
+}
 
 
 export default App;
